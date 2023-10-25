c503e9c04c9e1b48d3da5b0810ea9f51b6fa0524
yohan perez <jhoanperez825@gmail.com>
2023-10-25 14:45:16 -0400

    inicio de puntuacion

---
+++ fili/rx4.py
@@ -0,0 +1,15 @@
+
+print("contador de puntuacion")
+puntuacion= int(input("Digite su puntuacion"))
+neto = 2400
+Inaceptable=0
+Aceptable=0.4
+Meritorio=0.6 
+if puntuacion == Inaceptable:
+    print(f" la cantidad de dinero que usted va a ganar es de {Inaceptable}" )
+elif Aceptable == Aceptable:
+    print(f"la cantidad de dinero que usted va a ganar es de {Aceptable*2400}")
+elif Aceptable ==Meritorio:
+    print(f"la cantidad de dinero que usted va a ganar es de {Meritorio*2400}")
+else:
+    print("ERROR")
